154fb1b029a875acad2b54d68cc8ce60f129f000
k2zoom <k2zoom@mail.ru>
2026-03-20 19:58:55 +0300

    Доработан скрипт по парсингу и вставка данных в базу

--- generate_test_data/generate_data.py
+++ generate_test_data/generate_data.py
@@ -5,10 +5,13 @@ import json
 import logging
 import os
 from datetime import datetime
+import time
+from settings import CITIES
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 env = os.environ
 DATA_URL = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'
+
 
 load_dotenv()
 
@@ -23,22 +26,23 @@ def create_bd_connection():
 
 def prepare_for_db(data):
     if data:        
-        return (
+        return [(
             data['name'],
-            data['main'].get('temp'),
-            data['main'].get('feels_like'),
-            data['main'].get('humidity'),
-            data['main'].get('pressure'),
+            data['main'].get('temp',0),
+            data['main'].get('temp_min',0),
+            data['main'].get('temp_max',0),
+            data['main'].get('feels_like',0),
+            data['main'].get('humidity',0),
+            data['main'].get('pressure',0),
             data['weather'][0].get('description', '') if data['weather'] else '',
-            data['wind'].get('speed'),
-            data['wind'].get('gust'),
+            data['wind'].get('speed',0),
+            data['wind'].get('gust', 0),
             data.get('visibility'),
             data['clouds'].get('all', 0),
             datetime.fromtimestamp(data['dt']),
-            datetime.now(),
             datetime.fromtimestamp(data['sys']['sunrise']) if 'sys' in data and 'sunrise' in data['sys'] else None,
             datetime.fromtimestamp(data['sys']['sunset']) if 'sys' in data and 'sunset' in data['sys'] else None
-        )
+        )]
 
 def get_weather_data(city):
     url = DATA_URL.format(city, env.get("WEATHER_API_KEY"))
@@ -46,10 +50,20 @@ def get_weather_data(city):
     response.raise_for_status()
     return response.json()
 
-
 def main():
-    data=get_weather_data('Moscow')
-    logging.info(f"Source_data: {data}")
-    logging.info(f"Data: {prepare_for_db(data)}")
+    conn=create_bd_connection()
+    for city in CITIES:
+        data=get_weather_data(city)
+        time.sleep(1)
+        prepared_data = prepare_for_db(data)
+        logging.info(f'Weather data for {data}')
+        with conn.cursor() as cursor:
+            cursor.executemany('''
+            INSERT INTO weather(name,temp,temp_min,temp_max,feels_like,humidity,pressure,weather_description,wind_speed,
+            gust,visibility,clouds,rep_dt,sunrise_dt,sunset_dt) VALUES(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13,:14,:15)
+            ''',prepared_data)
+            conn.commit()
+            
+        logging.info(f"Source_data: {prepare_for_db(data)}")
 if __name__ == '__main__':
     main()
---
+++ generate_test_data/settings.py
@@ -0,0 +1,9 @@
+CITIES = [
+    "Moscow", "Saint Petersburg", "Novosibirsk", "Yekaterinburg", "Kazan",
+    "London", "Paris", "Berlin", "Madrid", "Rome", "Amsterdam", "Vienna",
+    "Prague", "Warsaw", "Budapest", "Athens", "Lisbon", "Stockholm", "Oslo",
+    "Helsinki", "Tokyo", "Beijing", "Seoul", "Shanghai", "Delhi", "Singapore",
+    "Bangkok", "Dubai", "Istanbul", "Jerusalem", "New York", "Los Angeles",
+    "Chicago", "Toronto", "Mexico City", "Sao Paulo", "Buenos Aires",
+    "Santiago", "Cairo", "Cape Town"
+] #40 городов, больше 200 000 городов всего
